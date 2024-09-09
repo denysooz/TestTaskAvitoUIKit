78bc8c887a06b2b38e960bca9a9a2d291e42f5d9
denysooz <den.darevskiy@mail.ru>
2024-09-09 17:37:08 +0300

    Initial Commit

---
+++ TestTaskAvitoUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTaskAvitoUIKit
+//
+//  Created by Denis Dareuskiy on 9.09.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
